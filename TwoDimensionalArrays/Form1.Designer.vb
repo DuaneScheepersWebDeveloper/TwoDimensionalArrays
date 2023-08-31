2ef093cf5feaac45e47a32deb9b3a07b39fc681d
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2023-08-31 09:48:26 +0200

    Initial commit

---
+++ TwoDimensionalArrays/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnGo1 = New Button()
+        btnGo2 = New Button()
+        btnGo3 = New Button()
+        btnGo4 = New Button()
+        SuspendLayout()
+        ' 
+        ' btnGo1
+        ' 
+        btnGo1.Location = New Point(78, 12)
+        btnGo1.Name = "btnGo1"
+        btnGo1.Size = New Size(191, 86)
+        btnGo1.TabIndex = 0
+        btnGo1.Text = "Display data items row wise"
+        btnGo1.UseVisualStyleBackColor = True
+        ' 
+        ' btnGo2
+        ' 
+        btnGo2.Location = New Point(78, 119)
+        btnGo2.Name = "btnGo2"
+        btnGo2.Size = New Size(191, 86)
+        btnGo2.TabIndex = 1
+        btnGo2.Text = "Display data items column wise"
+        btnGo2.UseVisualStyleBackColor = True
+        ' 
+        ' btnGo3
+        ' 
+        btnGo3.Location = New Point(78, 228)
+        btnGo3.Name = "btnGo3"
+        btnGo3.Size = New Size(191, 86)
+        btnGo3.TabIndex = 2
+        btnGo3.Text = "Display all data items at once"
+        btnGo3.UseVisualStyleBackColor = True
+        ' 
+        ' btnGo4
+        ' 
+        btnGo4.Location = New Point(78, 338)
+        btnGo4.Name = "btnGo4"
+        btnGo4.Size = New Size(191, 86)
+        btnGo4.TabIndex = 3
+        btnGo4.Text = "Search For a Person"
+        btnGo4.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnGo4)
+        Controls.Add(btnGo3)
+        Controls.Add(btnGo2)
+        Controls.Add(btnGo1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnGo1 As Button
+    Friend WithEvents btnGo2 As Button
+    Friend WithEvents btnGo3 As Button
+    Friend WithEvents btnGo4 As Button
+End Class
---
+++ TwoDimensionalArrays/Form1.vb
@@ -0,0 +1,116 @@
+﻿Public Class Form1
+    Dim astPeople(4, 5) As String
+    Dim x As Integer
+    Dim y As Integer
+    Public Sub InitializeData()
+
+        astPeople(0, 0) = "Barack"
+        astPeople(1, 0) = "Obama"
+        astPeople(2, 0) = "Male"
+        astPeople(3, 0) = "American"
+        astPeople(4, 0) = "President"
+
+        astPeople(0, 1) = "Jacinda"
+        astPeople(1, 1) = "Ardern"
+        astPeople(2, 1) = "Female"
+        astPeople(3, 1) = "New Zealand"
+        astPeople(4, 1) = "Prime Minister"
+
+        astPeople(0, 2) = "Ada"
+        astPeople(1, 2) = "Lovelace"
+        astPeople(2, 2) = "Female"
+        astPeople(3, 2) = "British"
+        astPeople(4, 2) = "Mathematician"
+
+        astPeople(0, 3) = "Albert"
+        astPeople(1, 3) = "Einstein"
+        astPeople(2, 3) = "Male"
+        astPeople(3, 3) = "Swiss"
+        astPeople(4, 3) = "Scientist"
+
+        astPeople(0, 4) = "Vincent"
+        astPeople(1, 4) = "van Gogh"
+        astPeople(2, 4) = "Male"
+        astPeople(3, 4) = "Dutch"
+        astPeople(4, 4) = "Artist"
+
+        astPeople(0, 5) = "Mahatma"
+        astPeople(1, 5) = "Ganghi"
+        astPeople(2, 5) = "Male"
+        astPeople(3, 5) = "Indian"
+        astPeople(4, 5) = "Lawyer"
+    End Sub
+    Private Sub btnGo1_Click(sender As Object, e As EventArgs) Handles btnGo1.Click
+
+        InitializeData()
+
+        For x = 0 To 4 ' Iterate through rows
+            Dim rowData As String = ""
+
+            For y = 0 To 5 ' Iterate through columns
+                rowData += astPeople(x, y) & " "
+            Next
+
+            MsgBox(rowData.Trim()) ' Display the row data
+        Next
+    End Sub
+
+
+    Private Sub btnGo2_Click(sender As Object, e As EventArgs) Handles btnGo2.Click
+
+        InitializeData()
+
+        For y = 0 To 5 ' Iterate through columns
+            Dim columnData As String = ""
+
+            For x = 0 To 4 ' Iterate through rows
+                columnData += astPeople(x, y) & vbCrLf ' Use vbCrLf for a new line
+            Next
+
+            MsgBox(columnData.Trim()) ' Display the column data
+        Next
+    End Sub
+
+    Private Sub btnGo3_Click(sender As Object, e As EventArgs) Handles btnGo3.Click
+
+        InitializeData()
+
+        Dim allData As String = ""
+
+        For x = 0 To 4 ' Iterate through rows
+            For y = 0 To 5 ' Iterate through columns
+                allData += astPeople(x, y) & " "
+            Next
+            allData += vbCrLf ' New line after each row
+        Next
+
+        MsgBox(allData.Trim()) ' Display all the data
+    End Sub
+
+    Private Sub btnGo4_Click(sender As Object, e As EventArgs) Handles btnGo4.Click
+        InitializeData()
+        Dim stTarget As String
+        Dim bFound As Boolean
+        bFound = False
+        Dim stOut As String
+        stTarget = InputBox("Enter the surname of the person you want to find")
+
+        For y = 0 To 5
+            If astPeople(1, y) = stTarget Then
+                bFound = True
+                Exit For
+            End If
+        Next
+
+        If bFound = True Then
+            For x = 0 To 4
+                stOut = stOut & astPeople(x, y) & " "
+            Next
+        Else
+            stOut = "Not Found"
+        End If
+
+        MsgBox(stOut)
+
+    End Sub
+End Class
